99cf17b630b96d1790ffa950bac66b5cf4cce946
bkis <boerge.kiss@gmail.com>
2015-03-24 14:08:07 +0100

    first prototype

--- JASEVAL/src/de/bkis/jaseval/JASEVAL.java
+++ JASEVAL/src/de/bkis/jaseval/JASEVAL.java
@@ -1,51 +1,64 @@
 package de.bkis.jaseval;
+
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.util.List;
+
 
 public class JASEVAL {
+	
+	
+	public SVDocument readSVFile(String path, String delimiter, boolean firstLineIsHeader) throws IOException{
+		System.out.print("[JASEVAL] reading " + path + "... ");
+		
+		SVDocument doc = new SVDocument();
+		FileReader fr = new FileReader(new File(path));
+		BufferedReader br = new BufferedReader(fr);
+		String currLine;
+		
+		System.out.print("(Detected File Encoding: " + fr.getEncoding() + ") ");
+		
+		//set header
+		if (firstLineIsHeader && (currLine = br.readLine()) != null){
+			doc.setHeader(br.readLine().split(delimiter));
+		}
+		
+		//read content
+		while ((currLine = br.readLine()) != null){
+			doc.addEntry(currLine, delimiter);
+		}
+		br.close();
 
-	public void writeSV(SVDocument svDoc, String delimiter, String path) {
+		System.out.println("- OK");
+		return doc;
+	}
+
+	public void writeSV(SVDocument doc, String delimiter, String path, String encoding) throws IOException {
 		System.out.print("[JASEVAL] writing data to " + path + " ... ");
 		StringBuilder sb = new StringBuilder();
 
 		// generate text data
-		for (String[] sArr : svData) {
-			for (String s : sArr) {
-				sb.append(s + delimiter);
-			}
+		doc.reset();
+		String[] currEntry;
+		while (doc.hasNextEntry()) {
+			currEntry = doc.nextEntry();
+			for (String s : currEntry) sb.append(s + delimiter);
 			sb.replace(sb.length() - delimiter.length(), sb.length(), "\n");
 		}
 
 		// write to file
+		if (encoding == null) encoding = "URF-8";
 		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(
-				new FileOutputStream(path), "UTF-8"));
+				new FileOutputStream(path), encoding));
 		out.write(sb.toString());
 		out.flush();
 		out.close();
 		
-		System.out.println("OK");
-	}
-
-	private String readFile(String path) throws IOException {
-		System.out.print("[JASEVAL] reading " + path + "... ");
-		StringBuffer sb = new StringBuffer();
-
-		FileReader fr = new FileReader(new File(path));
-		System.out.print("(Detected File Encoding: " + fr.getEncoding() + ") ");
-		int c;
-		while ((c = fr.read()) > -1){
-			sb.append((char) c);
-		}
-		fr.close();
-
-		System.out.println("OK");
-		return sb.toString();
+		System.out.println("- OK");
 	}
 
 }
--- JASEVAL/src/de/bkis/jaseval/SVDocument.java
+++ JASEVAL/src/de/bkis/jaseval/SVDocument.java
@@ -1,6 +1,7 @@
 package de.bkis.jaseval;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class SVDocument {
@@ -15,28 +16,51 @@ public class SVDocument {
 	}
 	
 	public String[] getEntry(int index){
-		//TODO
-		return null;
-	}
-	
-	public String[] getEntry(String field){
-		//check for header
-		if (header == null){
-			System.out.println("[JASEVAL] Error! No header specified.");
+		if (list.size() > index){
+			return list.get(index);
+		} else {
+			System.out.println("[JASEVAL] Error! Entry at index " + index + " doesn't exist!");
 			return null;
 		}
-		
-		//TODO
 	}
 	
-	public String[] getNextEntry(){
-		//TODO
-		return null;
+	public List<String[]> getEntriesContaining(String query){
+		List<String[]> results = new ArrayList<String[]>();
+		
+		for (String[] entry : list){
+			for (String s : entry){
+				if (s.contains(query)){
+					results.add(entry);
+					break;
+				}
+			}
+		}
+		
+		return results;
+	}
+	
+	public String[] nextEntry(){
+		String[] e;
+		
+		if (list.size() > currIndex){
+			e = list.get(currIndex);
+			currIndex++;
+			return e;
+		} else {
+			return null;
+		}
+	}
+	
+	public void reset(){
+		currIndex = 0;
 	}
 	
 	public boolean hasNextEntry(){
-		//TODO
-		return false;
+		if (list.size() > currIndex){
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
 	public void setHeader(String[] header){
@@ -44,16 +68,20 @@ public class SVDocument {
 	}
 	
 	public void addEntry(String entry, String delimiter){
-		String[] e = entry.split(delimiter);
-		list.add(e);
-		
-		//field count warnings
-		if (header != null && e.length != header.length){
-			System.out.println("[JASEVAL] Warning! Added entries field count "
+		String[] e = entry.split(delimiter, -1);
+		addEntry(e);
+	}
+	
+	public void addEntry(String[] entry){
+		//check field count
+		if (header != null && entry.length != header.length){
+			System.out.println("[JASEVAL] Error! Added entries field count "
 					+ "doesn't match header length: \"" + entry + "\"");
-		} else if (list.size() > 0 && e.length != list.get(list.size()-1).length) {
+		} else if (list.size() > 0 && entry.length != list.get(list.size()-1).length) {
 			System.out.println("[JASEVAL] Warning! Added entries field count "
 					+ "doesn't match existing entries: \"" + entry + "\"");
+		} else {
+			list.add(entry);
 		}
 	}
 	
@@ -63,6 +91,19 @@ public class SVDocument {
 		} else {
 			System.out.println("[JASEVAL] Error! Entry at index " + index + " could not be removed / doesn't exist!");
 			return null;
+		}
+	}
+	
+	public int getFieldIndex(String field){
+		if (header == null){
+			System.out.println("[JASEVAL] Error! No header specified.");
+			return -1;
+		} else {
+			for (int i = 0; i < header.length; i++) {
+				if (header[i].equals(field)) return i;
+			}
+			System.out.println("[JASEVAL] Error! Field \"" + field + "\" not found.");
+			return -1;
 		}
 	}
 
